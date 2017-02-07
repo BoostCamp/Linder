1e85abb779399b4d5bbe10e0dcd0ca2c7bcd18bb
박종훈 <norary3@gmail.com>
2017-02-07 15:24:53 +0900

    Project initialize

---
+++ Linder/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Linder
+//
+//  Created by 박종훈 on 2017. 2. 7..
+//  Copyright © 2017년 Linder. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
